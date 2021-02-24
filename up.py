ddd13e557505d758e36386c30e30352fd7eb80bf
Ch0l0 <73145048+ch0l0hatwhite@users.noreply.github.com>
2021-02-24 13:37:10 -0600

    Add files via upload

---
+++ up.py
@@ -0,0 +1,35 @@
+import os
+import time
+
+print(' Bienvenidos a UDT')
+time.sleep(2)
+
+print('cargando...')
+time.sleep(3)
+
+
+	
+os.system(" clear")
+os.system("apt update")
+os.system("pkg install mc ")
+os.system("pkg install python2 -y")
+os.system("pkg install ruby -y")
+os.system("pkg install cmatrix -y")
+os.system("pkg install nano -y")
+os.system("pkg install php -y")
+os.system("pkg install wget -y")
+os.system("pkg install curl -y")
+os.system("pkg install sqlmap -y")
+os.system("pkg install irssi -y")
+os.system("pkg install root-repo ")
+os.system("pkg install x11-repo ")
+os.system("pkg install unstable-repo")
+os.system("pkg update ")
+os.system("nmap -y")
+os.system("pkg install openssh -y")
+meta = input("la siguiente herramienta en instalarse es Framework Metasploit y Tiene un peso de 3gb Desea continuar? y / n  >> ")
+if meta == "y":
+	os.system("pkg install metasploit")
+	
+elif meta == "n":
+	os.system("exit")
